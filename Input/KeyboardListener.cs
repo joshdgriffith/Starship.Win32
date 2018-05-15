77e24fd6166beda2c79164685671aff450a7f83a
Josh Griffith <josh@starshipconsulting.com>
2018-05-15 07:41:09 -0400

    add KeyboardListener and move ImageExtensions

---
+++ Input/KeyboardListener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+using Gma.System.MouseKeyHook;
+
+namespace Starship.Win32.Input {
+    public static class KeyboardListener {
+
+        static KeyboardListener() {
+            Events = Hook.GlobalEvents();
+        }
+
+        public static void Start() {
+            Events.KeyPress += OnKeyPress;
+        }
+
+        public static void Stop() {
+            Events.KeyPress -= OnKeyPress;
+        }
+
+        private static void OnKeyPress(object sender, KeyPressEventArgs e) {
+            KeyPressed?.Invoke(e);
+        }
+
+        public static event Action<KeyPressEventArgs> KeyPressed;
+
+        private static IKeyboardMouseEvents Events { get; set; }
+    }
+}
